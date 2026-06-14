e8feb9e3b13fa88248d6c8b8715e424e3855dd8f
Sentot Ali Basah <147478871+sentotAB@users.noreply.github.com>
2026-06-14 16:44:13 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-set -e  # Kalau ada error, langsung stop biar nggak buang waktu
-
-echo "Downloading Flutter SDK..."
+set -e
 git clone https://github.com/flutter.git -b stable --depth 1
-
-echo "Set PATH..."
-export PATH="$PATH:$(pwd)/flutter/bin"
-
-echo "Flutter doctor..."
-flutter doctor -v
-
-echo "Get packages..."
-flutter pub get
-
-echo "Build Web..."
-flutter build web --release --base-href /
-
-echo "Build Selesai! Folder ada di build/web"
+flutter/bin/flutter doctor -v
+flutter/bin/flutter pub get
+flutter/bin/flutter build web --release --base-href /
